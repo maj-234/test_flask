2739b6ba88a5444669b8f36510eedb4dcc5ebdf2
qcloud <ubuntu@localhost.localdomain>
2024-04-18 21:19:57 +0800

    update

---
+++ notetake/blog.py
@@ -0,0 +1,20 @@
+from flask import Blueprint, render_template
+from .db import get_db
+
+bp = Blueprint('blog', __name__, url_prefix="/blog")
+
+bp.route('/')
+def index():
+    db = get_db()
+    posts = db.execute(
+        'SELECT post.id, title, body, created, author_id, usernaem'
+        'FROM post INNER JOIN user ON post.author_id = user.id'
+        'ORDER BY created DESC'
+    ).fetchall()
+    return render_template('blog/index/html', posts=posts)
+
+
+bp.route('/create', methods=("GET", "POST"))
+def create():
+    db = get_db()
+    
